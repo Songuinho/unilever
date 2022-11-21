0b3e3d2d1af29cdff6e1904256afdd517357731b
njammy <njammypatchakwejovani@gmail.com>
2022-11-21 20:23:00 +0100

    html struct define ok

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,62 @@
+<section class="container">
+    <div>
+        <img src="" alt="">
+        <div>Partagez cette page</div>
+        <img src="" alt="">
+        <img src="" alt="">
+        <img src="" alt="">
+        <img src="" alt="">
+    </div>
+    <div>
+        <div>
+            <h1>Connectez-vous à nous</h1>
+            <div>
+                Nous cherchons toujours à garder le contact avec les personnes pour qui un avenir durable est important.
+            </div>
+            <div>
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+            </div>
+        </div>
+        <div>
+            <h1>Nous joindre</h1>
+            <div>
+                Contactez Unilever et ses équipes de spécialistes ou trouvez des contacts du monde entier.
+            </div>
+            <div>
+                <div>Nous joindre</div>
+                <div><img src="" alt=""></div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div>
+            <div>Unilever France</div>
+            <div>© Unilever 2022</div>
+            <div>
+                <a href="">Contactez-nous</a>
+                <a href="">Qu'y a-t-il dans nos produits ?</a>
+                <a href="">Mentions légales</a>
+                <a href="">Accessibilité</a>
+                <a href="">Avis relatif aux Cookies</a>
+                <a href="">Avis de Confidentialite</a>
+                <a href="">Authenticité des avis</a>
+                <a href="">Authenticité des avis</a>
+                <a href="">Sitemap</a>
+                <a href="">Recyclage</a>
+            </div>
+        </div>
+        <div>
+            <img src="" alt="">
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
